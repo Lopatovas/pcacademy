0b90223a575145475264046450d3a1ad6f451ce7
Lopatovas <39686486+Lopatovas@users.noreply.github.com>
2018-11-15 20:11:16 +0200

    Pataisyta užduotis

--- TextArea homework task/script/areaBehaviour.js
+++ TextArea homework task/script/areaBehaviour.js
@@ -1,11 +1,9 @@
-/* eslint-disable brace-style */
-/* eslint-disable no-unused-vars */
-
+// eslint-disable-next-line no-unused-vars
 function areaControl () {
   let minRows = 5;
   let maxRows = 10;
 
-  function updateVariables () {
+  function updateMinMaxRows () {
     let minimumInput = document.getElementById('minimum');
     let maximumInput = document.getElementById('maximum');
     let tempMinRows = Number(minimumInput.value);
@@ -14,16 +12,14 @@ function areaControl () {
       showMessage('Error, incorrect parameters were passed. Make sure the inputs are positive and Minimum < Maximum');
       minimumInput.value = minRows;
       maximumInput.value = maxRows;
-    }
-    else if (tempMinRows % 1 != 0 || tempMaxRows % 1 != 0) {
+    } else if (tempMinRows % 1 !== 0 || tempMaxRows % 1 !== 0) {
       showMessage('Error, incorrect parameters were passed. Make sure the values are full numbers');
       minRows = Math.round(tempMinRows);
       maxRows = Math.round(tempMaxRows);
       minimumInput.value = minRows;
       maximumInput.value = maxRows;
       updateArea();
-    } 
-    else {
+    } else {
       minRows = tempMinRows;
       maxRows = tempMaxRows;
       updateArea();
@@ -39,28 +35,29 @@ function areaControl () {
     for (let i = 0; i < seperateLines.length; i++) {
       if (seperateLines[i].length > charsPairRow) {
         rowCounter = rowCounter + Math.ceil(Number(seperateLines[i].length) / charsPairRow);
-      }
-      else {
+      } else {
         rowCounter = rowCounter + 1;
       }
     }
     if (rowCounter > minRows && rowCounter <= maxRows) {
       textArea.rows = rowCounter;
-    }
-    else if (rowCounter <= minRows) {
+    } else if (rowCounter <= minRows) {
       textArea.rows = minRows;
-    }
-    else {
+    } else {
       textArea.rows = maxRows;
     }
   }
 
-  function showMessage(text) {
-    let message = document.getElementById("message");
+  function showMessage (text) {
+    let message = document.getElementById('message');
     message.innerHTML = text;
-    message.className = "show";
-    setTimeout(function(){ message.className = message.className.replace("show", ""); }, 3000);
+    message.className = 'show';
+    setTimeout(disableToast(message), 3000);
   }
 
-  updateVariables();
+  function disableToast (message) {
+    message.className = message.className.replace('show', '');
+  }
+
+  updateMinMaxRows();
 }
