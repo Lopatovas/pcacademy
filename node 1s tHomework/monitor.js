e5ed6ed26353c74e60ea5411813595214a3559ec
Lopatovas <39686486+Lopatovas@users.noreply.github.com>
2018-11-23 12:50:47 +0200

    Finished homework task

--- node 1s tHomework/monitor.js
+++ node 1s tHomework/monitor.js
@@ -1,13 +1,19 @@
-const ComputerInfo = require('./ComputerInfo');
-const constantFile = require('./const');
+const ComputerInfo = require('./source/ComputerInfo');
+const constantFile = require('./source/const');
 
-function printInformation(){
-    let computerInfo = new ComputerInfo();
+let computerInfo = new ComputerInfo();
 
-    computerInfo.printCpuUsage();
+function printPCInformation(){
     computerInfo.printMemUsage();
+    computerInfo.printCpuUsage();
 
-    setTimeout(printInformation, constantFile.TIME_TO_RECALCULATE_DATA);
+    setTimeout(printPCInformation, constantFile.TIME_TO_RECALCULATE_DATA);
 }
 
-printInformation();
+function printUserInformation(){
+    console.log(`Resource monitoring started. Press ${constantFile.EXIT_KEY} to exit.`);
+    console.log();
+}
+
+printUserInformation();
+printPCInformation();
--- node 1s tHomework/source/ComputerInfo.js
+++ node 1s tHomework/source/ComputerInfo.js
@@ -2,17 +2,19 @@ const os = require('os-utils');
 const constantFile = require('./const');
 
 class ComputerInfo{
+
+    printMemUsage(){
+        let memFree = os.freememPercentage() * constantFile.CONVERSION_TO_PERCENTAGE;
+        let memUsage = parseFloat(constantFile.MAX_USED_MEM_PERCENTAGE - memFree).toFixed(constantFile.MAX_NUMBERS_AFTER_POINT);
+        setTimeout(function(){console.log(`Memory Usage: ${memUsage} %`)}, constantFile.TIME_TO_WAIT_FOR_CPU);
+    }
+
     printCpuUsage(){
         os.cpuUsage(function(cpuUsage){;
             cpuUsage = parseFloat(cpuUsage * constantFile.CONVERSION_TO_PERCENTAGE).toFixed(constantFile.MAX_NUMBERS_AFTER_POINT);
             console.log(`CPU Usage ${cpuUsage} %`);
+            console.log();
         });
-    }
-    
-    printMemUsage(){
-        let memFree = os.freememPercentage() * constantFile.CONVERSION_TO_PERCENTAGE;
-        let memUsage = parseFloat(constantFile.MAX_USED_MEM_PERCENTAGE - memFree).toFixed(constantFile.MAX_NUMBERS_AFTER_POINT);
-        console.log(`Memory Usage: ${memUsage} %`);
     }
 }
 
