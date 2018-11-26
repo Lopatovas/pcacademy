b644e233eee8363b4a3473ee00b85860893cbeea
Lopatovas <39686486+Lopatovas@users.noreply.github.com>
2018-11-26 20:36:16 +0200

    Minor fixes

--- Loan homework task (typescript)/source/consumerLoan.ts
+++ Loan homework task (typescript)/source/consumerLoan.ts
@@ -6,7 +6,7 @@ export default class ConsumerLoan extends Loan{
     maxAmount: number = 10000;
     maxTime: number = 60;
 
-    constructor(amount:number, time:number){
-        super(amount, time, constant.FAST_LOAN_INTEREST_RATE);
+    constructor(amount:number, time:number, interestRate:number){
+        super(amount, time, interestRate);
     }
 }
